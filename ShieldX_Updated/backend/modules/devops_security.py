80f3378d6725c5cd9184bbc407e158b44b11f1a8
overlord2077-voldigoad <vitaseptem.e@gmail.com>
2025-01-14 21:41:48 -0300

    Delete ShieldX_Updated directory

--- ShieldX_Updated/backend/modules/devops_security.py
+++
@@ -1,7 +0,0 @@
-class DevOpsSecurity:
-    def __init__(self):
-        self.vulnerabilities = []
-
-    def analyze_pipeline(self, pipeline_name):
-        self.vulnerabilities.append({"pipeline": pipeline_name, "issues": ["Hardcoded Secrets", "Open Ports"]})
-        return {"pipeline": pipeline_name, "issues": ["Hardcoded Secrets", "Open Ports"], "message": "Análise realizada."}
