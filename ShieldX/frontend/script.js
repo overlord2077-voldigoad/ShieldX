cda956afd43fc47212f42b588965111db5754102
overlord2077-voldigoad <vitaseptem.e@gmail.com>
2025-01-14 21:54:16 -0300

    Delete ShieldX directory

--- ShieldX/frontend/script.js
+++
@@ -1,8 +0,0 @@
-function loadThreatPredictions() {
-    fetch("/predict-threats")
-        .then(response => response.json())
-        .then(data => {
-            const resultDiv = document.getElementById("result");
-            resultDiv.innerHTML = `<h3>Ameaças Previstos:</h3><ul>${data.predicted_threats.map(t => `<li>${t.threat_type}: ${t.likelihood}</li>`).join('')}</ul>`;
-        });
-}
